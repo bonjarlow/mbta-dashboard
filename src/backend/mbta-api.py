48df35fa33fc4f51eb60b0ff0d25a9365f1fe2fb
bonjarlow <jon.michael.barlow@gmail.com>
2025-07-06 14:29:44 -0400

    clarify run argument

--- src/backend/mbta-api.py
+++ src/backend/mbta-api.py
@@ -16,4 +16,5 @@ def get_trains():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
+    # allow external connections (host 0.0.0.0)
     app.run(host='0.0.0.0', port=5000, debug=True)
